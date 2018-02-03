fce6c1e1d972b51c222c1eaaef1a3d5e8ef0e94b
Hekmon <edouardhur@gmail.com>
2018-02-03 19:55:08 +0100

    switch to int64

--- torrent-get.go
+++ torrent-get.go
@@ -21,7 +21,7 @@ func (c *Controller) TorrentGetAll() (torrents []*Torrent, err error) {
 }
 
 // TorrentGet returns the given of fields (mandatory) for each ids (optionnal)
-func (c *Controller) TorrentGet(fields []string, ids []int) (torrents []*Torrent, err error) {
+func (c *Controller) TorrentGet(fields []string, ids []int64) (torrents []*Torrent, err error) {
 	// Validate fields
 	var fieldInvalid bool
 	var knownField string
@@ -42,7 +42,7 @@ func (c *Controller) TorrentGet(fields []string, ids []int) (torrents []*Torrent
 	return c.torrentGet(fields, ids)
 }
 
-func (c *Controller) torrentGet(fields []string, ids []int) (torrents []*Torrent, err error) {
+func (c *Controller) torrentGet(fields []string, ids []int64) (torrents []*Torrent, err error) {
 	arguments := torrentGetParams{
 		Fields: fields,
 		IDs:    ids,
@@ -58,7 +58,7 @@ func (c *Controller) torrentGet(fields []string, ids []int) (torrents []*Torrent
 
 type torrentGetParams struct {
 	Fields []string `json:"fields"`
-	IDs    []int    `json:"ids,omitempty"`
+	IDs    []int64  `json:"ids,omitempty"`
 }
 
 type torrentGetResults struct {
@@ -66,79 +66,79 @@ type torrentGetResults struct {
 }
 
 // Torrent represents all the possible fields of data for a torrent
-// All fields are pointers to detect if the value is nil (field not requested) or default real default value
+// All fields are point64ers to detect if the value is nil (field not requested) or default real default value
 // https://trac.transmissionbt.com/browser/tags/2.92/extras/rpc-spec.txt?rev=14714#L148
 type Torrent struct {
-	ActivityDate            *int               `json:"activityDate"`
-	AddedDate               *int               `json:"addedDate"`
-	BandwidthPriority       *int               `json:"bandwidthPriority"`
+	ActivityDate            *int64             `json:"activityDate"`
+	AddedDate               *int64             `json:"addedDate"`
+	BandwidthPriority       *int64             `json:"bandwidthPriority"`
 	Comment                 *string            `json:"comment"`
-	CorruptEver             *int               `json:"corruptEver"`
+	CorruptEver             *int64             `json:"corruptEver"`
 	Creator                 *string            `json:"creator"`
-	DateCreated             *int               `json:"dateCreated"`
-	DesiredAvailable        *int               `json:"desiredAvailable"`
-	DoneDate                *int               `json:"doneDate"`
+	DateCreated             *int64             `json:"dateCreated"`
+	DesiredAvailable        *int64             `json:"desiredAvailable"`
+	DoneDate                *int64             `json:"doneDate"`
 	DownloadDir             *string            `json:"downloadDir"`
-	DownloadedEver          *int               `json:"downloadedEver"`
-	DownloadLimit           *int               `json:"downloadLimit"`
+	DownloadedEver          *int64             `json:"downloadedEver"`
+	DownloadLimit           *int64             `json:"downloadLimit"`
 	DownloadLimited         *bool              `json:"downloadLimited"`
-	Error                   *int               `json:"error"`
+	Error                   *int64             `json:"error"`
 	ErrorString             *string            `json:"errorString"`
-	Eta                     *int               `json:"eta"`
-	EtaIdle                 *int               `json:"etaIdle"`
+	Eta                     *int64             `json:"eta"`
+	EtaIdle                 *int64             `json:"etaIdle"`
 	Files                   []*TorrentFile     `json:"files"`
 	FileStats               []*TorrentFileStat `json:"fileStats"`
 	HashString              *string            `json:"hashString"`
-	HaveUnchecked           *int               `json:"haveUnchecked"`
-	HaveValid               *int               `json:"haveValid"`
+	HaveUnchecked           *int64             `json:"haveUnchecked"`
+	HaveValid               *int64             `json:"haveValid"`
 	HonorsSessionLimits     *bool              `json:"honorsSessionLimits"`
-	ID                      *int               `json:"id"`
+	ID                      *int64             `json:"id"`
 	IsFinished              *bool              `json:"isFinished"`
 	IsPrivate               *bool              `json:"isPrivate"`
 	IsStalled               *bool              `json:"isStalled"`
-	LeftUntilDone           *int               `json:"leftUntilDone"`
+	LeftUntilDone           *int64             `json:"leftUntilDone"`
 	MagnetLink              *string            `json:"magnetLink"`
-	ManualAnnounceTime      *int               `json:"manualAnnounceTime"`
-	MaxConnectedPeers       *int               `json:"maxConnectedPeers"`
+	ManualAnnounceTime      *int64             `json:"manualAnnounceTime"`
+	MaxConnectedPeers       *int64             `json:"maxConnectedPeers"`
 	MetadataPercentComplete *float64           `json:"metadataPercentComplete"`
 	Name                    *string            `json:"name"`
-	PeerLimit               *int               `json:"peer-limit"`
+	PeerLimit               *int64             `json:"peer-limit"`
 	Peers                   []*Peer            `json:"peers"`
 }
 
 // TorrentFile represent one file from a Torrent
 // https://trac.transmissionbt.com/browser/tags/2.92/extras/rpc-spec.txt?rev=14714#L221
 type TorrentFile struct {
-	BytesCompleted int    `json:"bytesCompleted"`
-	Length         int    `json:"length"`
+	BytesCompleted int64  `json:"bytesCompleted"`
+	Length         int64  `json:"length"`
 	Name           string `json:"name"`
 }
 
 // TorrentFileStat represents the metadata of a torrent's file
 // https://trac.transmissionbt.com/browser/tags/2.92/extras/rpc-spec.txt?rev=14714#L227
 type TorrentFileStat struct {
-	BytesCompleted int  `json:"bytesCompleted"`
-	Wanted         bool `json:"wanted"`
-	Priority       int  `json:"priority"`
+	BytesCompleted int64 `json:"bytesCompleted"`
+	Wanted         bool  `json:"wanted"`
+	Priority       int64 `json:"priority"`
 }
 
 // Peer represent a peer metadata of a torrent's peer list
 // https://trac.transmissionbt.com/browser/tags/2.92/extras/rpc-spec.txt?rev=14714#L235
 type Peer struct {
-	Address            string  `json:"address"`
-	ClientName         string  `json:"clientName"`
-	ClientIsChoked     bool    `json:"clientIsChoked"`
-	ClientIsInterested bool    `json:"clientIsInterested"`
-	FlagStr            string  `json:"flagStr"`
-	IsDownloadingFrom  bool    `json:"isDownloadingFrom"`
-	IsEncrypted        bool    `json:"isEncrypted"`
-	IsIncoming         bool    `json:"isIncoming"`
-	IsUploadingTo      bool    `json:"isUploadingTo"`
-	IsUTP              bool    `json:"isUTP"`
-	PeerIsChoked       bool    `json:"peerIsChoked"`
-	PeerIsInterested   bool    `json:"peerIsInterested"`
-	Port               int     `json:"port"`
-	Progress           float64 `json:"progress"`
-	RateToClient       int     `json:"rateToClient"`
-	RateToPeer         int     `json:"rateToPeer"`
+	Address              string  `json:"address"`
+	ClientName           string  `json:"clientName"`
+	ClientIsChoked       bool    `json:"clientIsChoked"`
+	ClientIsint64erested bool    `json:"clientIsint64erested"`
+	FlagStr              string  `json:"flagStr"`
+	IsDownloadingFrom    bool    `json:"isDownloadingFrom"`
+	IsEncrypted          bool    `json:"isEncrypted"`
+	IsIncoming           bool    `json:"isIncoming"`
+	IsUploadingTo        bool    `json:"isUploadingTo"`
+	IsUTP                bool    `json:"isUTP"`
+	PeerIsChoked         bool    `json:"peerIsChoked"`
+	PeerIsint64erested   bool    `json:"peerIsint64erested"`
+	Port                 int64   `json:"port"`
+	Progress             float64 `json:"progress"`
+	RateToClient         int64   `json:"rateToClient"`
+	RateToPeer           int64   `json:"rateToPeer"`
 }
