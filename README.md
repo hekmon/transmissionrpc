8c91e9a2e07127c3048e39c104a29f3b7cc14291
Hekmon <edouardhur@gmail.com>
2018-02-10 12:17:06 +0100

    torrent set location done

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ https://trac.transmissionbt.com/browser/tags/2.92/extras/rpc-spec.txt?rev=14714
 
 #### Moving a Torrent
 
-* torrent-set-location _(to do)_
+* torrent-set-location _(done)_
 
 #### Renaming a Torrent's Path
 
---
+++ torrent_set_location.go
@@ -0,0 +1,36 @@
+package transmissionrpc
+
+import (
+	"errors"
+	"fmt"
+)
+
+/*
+	Moving a torrent
+	https://trac.transmissionbt.com/browser/tags/2.92/extras/rpc-spec.txt?rev=14714#L406
+*/
+
+// TorrentSetLocation allows to set a new location for one or more torrents.
+// https://trac.transmissionbt.com/browser/tags/2.92/extras/rpc-spec.txt?rev=14714#L408
+func (c *Controller) TorrentSetLocation(payload *TorrentSetLocationPayload) (err error) {
+	// Validate
+	if payload == nil {
+		return errors.New("payload can't be nil")
+	}
+	if len(payload.IDs) == 0 {
+		return errors.New("there must be at least one ID")
+	}
+	// Send payload
+	if err = c.rpcCall("torrent-set-location", payload, nil); err != nil {
+		err = fmt.Errorf("'torrent-set-location' rpc method failed: %v", err)
+	}
+	return
+}
+
+// TorrentSetLocationPayload describes the torrents' id(s) and other options.
+// https://trac.transmissionbt.com/browser/tags/2.92/extras/rpc-spec.txt?rev=14714#L412
+type TorrentSetLocationPayload struct {
+	IDs      []int64 `json:"ids"`      // torrent list
+	Location string  `json:"location"` // the new torrent location
+	Move     bool    `json:"move"`     // if true, move from previous location. Otherwise, search "location" for files
+}
