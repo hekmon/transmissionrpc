0a64072c5c75ea3803f7837a072f6d8065a89f47
Hekmon <edouardhur@gmail.com>
2018-03-03 23:59:23 +0100

    add doc

--- controller.go
+++ controller.go
@@ -9,6 +9,7 @@ import (
 )
 
 const (
+	// RPCVersion indicates the exact transmission RPC version this library is build against
 	RPCVersion       = 15
 	defaultPort      = 9091
 	defaultRPCPath   = "/transmission/rpc"
