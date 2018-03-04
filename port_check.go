6af2a292f305f46ec1a4c0e25c461afa42a4aff7
Hekmon <edouardhur@gmail.com>
2018-03-04 14:17:14 +0100

    reorganize port_check

--- port_check.go
+++ port_check.go
@@ -9,18 +9,18 @@ import (
 	https://trac.transmissionbt.com/browser/tags/2.92/extras/rpc-spec.txt?rev=14714#L569
 */
 
-// CheckPort allow to see if your incoming peer port is accessible from the outside world.
-func (c *Client) CheckPort() (opened bool, err error) {
+// CheckPort allows tests to see if your incoming peer port is accessible from the outside world.
+func (c *Client) CheckPort() (open bool, err error) {
 	var result transmissionCheckPortAnswer
 	// Send request
-	if err = c.rpcCall("port-test", nil, &result); err != nil {
+	if err = c.rpcCall("port-test", nil, &result); err == nil {
+		open = result.PortOpen
+	} else {
 		err = fmt.Errorf("'port-test' rpc method failed: %v", err)
-		return
 	}
-	opened = result.PortOpened
 	return
 }
 
 type transmissionCheckPortAnswer struct {
-	PortOpened bool `json:"port-is-open"`
+	PortOpen bool `json:"port-is-open"`
 }
