154e7c162d071ef740568d14588fc86204ab7e06
Константин <primebit@MacBook-Pro-Konstantin.local>
2018-12-26 21:40:33 +0300

    Added support for torrent-rename-path method

--- torrent_rename_path.go
+++ torrent_rename_path.go
@@ -1,6 +1,36 @@
 package transmissionrpc
 
+import (
+	"errors"
+	"fmt"
+)
+
 /*
-	Renaming a Torrent's Path
+	Rename a torrent path
 	https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L438
 */
+
+// TorrentRenamePath allows to rename torrent name or path.
+// https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L438
+func (c *Client) TorrentRenamePath(payload *TorrentRenamePathPayload) (err error) {
+	// Validate
+	if payload == nil {
+		return errors.New("payload can't be nil")
+	}
+	if len(payload.IDs) == 0 {
+		return errors.New("there must be at least one ID")
+	}
+	// Send payload
+	if err = c.rpcCall("torrent-rename-path", payload, nil); err != nil {
+		err = fmt.Errorf("'torrent-rename-path' rpc method failed: %v", err)
+	}
+	return
+}
+
+// TorrentSetLocationPayload describes the torrents' id(s) and other options.
+// https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L427
+type TorrentRenamePathPayload struct {
+	IDs      []int64	`json:"ids"`	// torrent list
+	Path 	 string 	`json:"path"`	// the new torrent path
+	Name     string		`json:"move"`	// new torrent name
+}
