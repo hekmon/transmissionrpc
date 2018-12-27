f48a314e1b09a146a213daaa8a506dd9899687d1
Hekmon <edouardhur@gmail.com>
2018-12-27 11:52:29 +0100

    uniformize TorrentRenamePath

--- torrent_rename_path.go
+++ torrent_rename_path.go
@@ -11,14 +11,14 @@ import (
 */
 
 // TorrentRenamePath allows to rename torrent name or path.
-// https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L438
+// https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L440
 func (c *Client) TorrentRenamePath(payload *TorrentRenamePathPayload) (err error) {
 	// Validate
 	if payload == nil {
 		return errors.New("payload can't be nil")
 	}
-	if len(payload.IDs) == 0 {
-		return errors.New("there must be at least one ID")
+	if len(payload.IDs) != 1 {
+		return errors.New("there must be one and only one ID")
 	}
 	// Send payload
 	if err = c.rpcCall("torrent-rename-path", payload, nil); err != nil {
@@ -27,10 +27,10 @@ func (c *Client) TorrentRenamePath(payload *TorrentRenamePathPayload) (err error
 	return
 }
 
-// TorrentSetLocationPayload describes the torrents' id(s) and other options.
-// https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L427
+// TorrentRenamePathPayload describes the torrents' id(s) and other options.
+// https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L447
 type TorrentRenamePathPayload struct {
-	IDs      []int64	`json:"ids"`	// torrent list
-	Path 	 string 	`json:"path"`	// the new torrent path
-	Name     string		`json:"name"`	// new torrent name
+	IDs  []int64 `json:"ids"`  // the torrent torrent list, as described in 3.1 (must only be 1 torrent)
+	Path string  `json:"path"` // the path to the file or folder that will be renamed
+	Name string  `json:"name"` // the file or folder's new name
 }
