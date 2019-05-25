ef1f9975f5fb83f94919bf8d344ca1c8624b50c1
Òscar Casajuana <elboletaire@underave.net>
2019-05-25 13:46:45 +0200

    Adding methods to retrieve and alter torrents by hash (#5)
    refs #4

--- torrent_set_location.go
+++ torrent_set_location.go
@@ -27,10 +27,35 @@ func (c *Client) TorrentSetLocation(payload *TorrentSetLocationPayload) (err err
 	return
 }
 
+// TorrentSetLocationHash allows to set a new location for one or more torrents.
+// https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L423
+func (c *Client) TorrentSetLocationHash(payload *TorrentSetLocationHashPayload) (err error) {
+	// Validate
+	if payload == nil {
+		return errors.New("payload can't be nil")
+	}
+	if len(payload.IDs) == 0 {
+		return errors.New("there must be at least one ID")
+	}
+	// Send payload
+	if err = c.rpcCall("torrent-set-location", payload, nil); err != nil {
+		err = fmt.Errorf("'torrent-set-location' rpc method failed: %v", err)
+	}
+	return
+}
+
 // TorrentSetLocationPayload describes the torrents' id(s) and other options.
 // https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L427
 type TorrentSetLocationPayload struct {
 	IDs      []int64 `json:"ids"`      // torrent list
 	Location string  `json:"location"` // the new torrent location
 	Move     bool    `json:"move"`     // if true, move from previous location. Otherwise, search "location" for files
+}
+
+// TorrentSetLocationHashPayload describes the torrent id (by hash) and other options.
+// https://github.com/transmission/transmission/blob/2.9x/extras/rpc-spec.txt#L427
+type TorrentSetLocationHashPayload struct {
+	IDs      []string `json:"ids"`      // torrent list
+	Location string   `json:"location"` // the new torrent location
+	Move     bool     `json:"move"`     // if true, move from previous location. Otherwise, search "location" for files
 }
